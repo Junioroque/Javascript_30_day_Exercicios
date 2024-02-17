62e3e51430e9cb15b14deddc8a2012c672c94fd1
Junioroque <jcjunioroque@gmail.com>
2024-02-17 18:10:11 -0300

    Primeiro Commit

---
+++ exercicio_dia_1/1.js
@@ -0,0 +1,3 @@
+// Escreva um comentário de uma única linha que diga: comentários podem tornar o código legível.
+console.log(`Podemos deixar o codigo mais legivel com os nomes da varaivel e funções mais detalhada, 
+e colocando (//) para comentar a sintaxe que está usando no codigo.`)

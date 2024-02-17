ef626a2f3764b774712823b21e9e2a955fb167fd
Junioroque <jcjunioroque@gmail.com>
2024-02-17 18:58:30 -0300

    exercicio 3

--- exercicio_dia_1/ex_2.js
+++ exercicio_dia_1/ex_2.js
@@ -1,2 +1,2 @@
 // Escreva outro comentário que diga: Bem-vindo ao 30DaysOfJavaScript
-console.log(`Bem-vindo ao 30DaysOfJavaScript`)
+console.log(`  Bem-vindo ao 30DaysOfJavaScript`)
--- exercicio_dia_1/ex_3.js
+++ exercicio_dia_1/ex_3.js
@@ -1,2 +1,6 @@
-// Crie um arquivo variável.js e declare variáveis ​​e atribua tipos de dados string, booleanos, indefinidos e nulos
+// Escreva um comentário de várias linhas que diga que os comentários podem tornar o código legível, fácil de reutilizar e informativo
+console.log(`  Os comentário pode ser linha que utiliza a (\\), e multiplas linhas usando (/* */).`)
+console.log(`  Os comentário pode sim ajudar a reconhecer o que uma sintaxe pode estar execundo ou 
+finalizando uma soma, subtração, multiplicação ou divisão.
+  Quando for corrigir um error ou fazer uma manlutenção no codigo. `)
 
